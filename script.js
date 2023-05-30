7d167e4c05ab0ca36c5ef2dd05bf6871c7e9aad5
mqasimkhan143 <113086699+mqasimkhan143@users.noreply.github.com>
2023-05-30 11:47:14 +0400

    Summary

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-function opentab(tabname){
-    for(tablink of tablinks){
+var tablinks = document.getElementsByClassName("tab-links"); //Fetching active tab link
+var tabcontents = document.getElementsByClassName("tab-contents"); //Fetching active 
+function opentab(tabname){ //Function to remove active div class to add it to the one user clicks to show contents of that div class
+    for(tablink of tablinks){//removing the first active class 
         tablink.classList.remove("active-link");
     }
-    for(tabcontent of tabcontents){
+    for(tabcontent of tabcontents){//removing the second active class
         tabcontent.classList.remove("active-tab");
     }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+    event.currentTarget.classList.add("active-link");//adding to the chosen class
+    document.getElementById(tabname).classList.add("active-tab");//adding to second chosen class
 }
